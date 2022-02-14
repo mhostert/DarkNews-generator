ad50c7affb39d174552bb8bda6ef9a8c35dfad9c
Qubitol <d.massaro.26@gmail.com>
2022-02-14 18:10:53 +0100

    Fix grammar order of push first and add tests

--- src/DarkNews/ExpressionParser.py
+++ src/DarkNews/ExpressionParser.py
@@ -80,8 +80,8 @@ class ExpressionParser:
             addop    :: '+' | '-'
             equalop  :: '='
             integer  :: ['+' | '-'] '0'..'9'+
-            variable :: [ a-zA-Z_ ][ a-zA-Z0-9_ ]*
-            atom     :: PI | E | real | fn '(' expr ')' | '(' expr ')' | variable
+            ident    :: [ a-zA-Z_ ][ a-zA-Z0-9_ ]*
+            atom     :: PI | E | real | fn '(' expr ')' | '(' expr ')'
             factor   :: atom [ expop factor ]*
             term     :: factor [ multop factor ]*
             expr     :: variable equalop term [ addop term ]*
@@ -99,7 +99,6 @@ class ExpressionParser:
             # fnumber = ppc.number().addParseAction(lambda t: str(t[0]))
             fnumber = Regex(r"[+-]?\d+(?:\.\d*)?(?:[eE][+-]?\d+)?")
             ident = Word(alphas, alphanums + "_$") # this is the name of a function
-            variable = Word(alphas + "_", alphanums + '_') # this is the name of a variable
 
             plus, minus, mult, div = map(Literal, "+-*/")
             lpar, rpar = map(Suppress, "()")
@@ -123,7 +122,7 @@ class ExpressionParser:
                 addop[...]
                 + (
                     (fn_call | pi | e | fnumber | ident).setParseAction(self._push_first)
-                    | Group(lpar + expr + rpar) | variable.setParseAction(self._push_value)
+                    | Group(lpar + expr + rpar)
                 )
             ).setParseAction(self._push_unary_minus)
 
@@ -132,12 +131,15 @@ class ExpressionParser:
             factor = Forward()
             factor <<= atom + (expop + factor).setParseAction(self._push_first)[...]
             term = factor + (multop + factor).setParseAction(self._push_first)[...]
-            expr <<= variable.setParseAction(self._push_first) + equalop + term + (addop + term).setParseAction(self._push_first)[...]
-            self._bnf = expr
+            expr <<= term + (addop + term).setParseAction(self._push_first)[...] # differentiate from assignment in order to describe functions argument list
+            assignment_expr = Forward()
+            assignment_expr <<= (ident + equalop + expr).setParseAction(self._push_first) # _push_first should be given only to the full match otherwise if given when it matches singularly then it would repeat everything every time it backtraces
+            self._bnf = assignment_expr
         return self._bnf
 
     def _push_first(self, toks):
         self._current_stack.append(toks[0])
+        # print(self._current_stack, toks)
 
     def _push_value(self, toks):
         try:
@@ -173,7 +175,10 @@ class ExpressionParser:
             args = reversed([self._evaluate_variable(s) for _ in range(num_args)])
             return self.fn[op](*args)
         elif op[0].isalpha():
-            raise self.ParsingError(f"invalid identifier '{op}'")
+            try:
+                return self.parameters[op]
+            except KeyError:
+                raise self.ParsingError(f"Invalid identifier '{op}'")
         else:
             # try to evaluate as int first, then as float if int fails
             try:
@@ -181,14 +186,18 @@ class ExpressionParser:
             except ValueError:
                 return float(op)
 
-    def evaluate_stack(self):
+    def evaluate_stack(self, copy=False):
+        # if copy == True, then don't consume the stack
         # get the first element of the stack, which must be the variable name
-        var_name = self._current_stack.pop()
+        if copy:
+            stack = self._current_stack[:]
+        var_name = stack.pop()
         # consistency check: should not be equal to E, e, PI, pi, Pi, or any other function name
         if (var_name.lower() in ['e', 'pi']) or (var_name in self.fn.keys()):
             raise self.ParsingError(f"Variable name '{var_name}' not allowed.")
         # evaluate the rest of the stack and assign the result to the variable
-        self.parameters[var_name] = self._evaluate_variable(self._current_stack)
+        # print(var_name, stack)
+        self.parameters[var_name] = self._evaluate_variable(stack)
         return var_name, self.parameters[var_name]
 
     def parse_string(self, *args, **kwargs):
@@ -203,7 +212,7 @@ if __name__ == "__main__":
     def test(s, expected_name, expected_value):
         try:
             results = parser.parse_string(s, parseAll=True)
-            name, val = parser.evaluate_stack()
+            name, val = parser.evaluate_stack(copy=True)
         except ParseException as pe:
             print(s, "failed parse:", str(pe))
         except ExpressionParser.ParsingError as e:
@@ -213,51 +222,81 @@ if __name__ == "__main__":
                 print(s, "<=>", name, "=", val, results, "=>", parser._current_stack)
             else:
                 print(s, "<=>", name, "=", val, '!!!', expected_name, "=", expected_value, results, "=>", parser._current_stack)
+    
+    class TEST:
+        gD = 9.7543
+        A = gD * 9.7543
+        B = 9.7543**2 + gD
+        C = 9.7543 + 2 - 4*A
+        D = 9.7543 + math.sin(0.3)
+        E = D
+        alphaD = gD ** 2 / (4 * math.pi)
+        sinx = math.sin(math.pi / 3) - 8
 
-    test("9", 9)
-    test("-9", -9)
-    test("--9", 9)
-    test("-E", -math.e)
-    test("9 + 3 + 6", 9 + 3 + 6)
-    test("9 + 3 / 11", 9 + 3.0 / 11)
-    test("(9 + 3)", (9 + 3))
-    test("(9+3) / 11", (9 + 3.0) / 11)
-    test("9 - 12 - 6", 9 - 12 - 6)
-    test("9 - (12 - 6)", 9 - (12 - 6))
-    test("2*3.14159", 2 * 3.14159)
-    test("3.1415926535*3.1415926535 / 10", 3.1415926535 * 3.1415926535 / 10)
-    test("PI * PI / 10", math.pi * math.pi / 10)
-    test("PI*PI/10", math.pi * math.pi / 10)
-    test("PI^2", math.pi ** 2)
-    test("round(PI^2)", round(math.pi ** 2))
-    test("6.02E23 * 8.048", 6.02e23 * 8.048)
-    test("e / 3", math.e / 3)
-    test("sin(PI/2)", math.sin(math.pi / 2))
-    test("10+sin(PI/4)^2", 10 + math.sin(math.pi / 4) ** 2)
-    test("trunc(E)", int(math.e))
-    test("trunc(-E)", int(-math.e))
-    test("round(E)", round(math.e))
-    test("round(-E)", round(-math.e))
-    test("E^PI", math.e ** math.pi)
-    test("exp(0)", 1)
-    test("exp(1)", math.e)
-    test("2^3^2", 2 ** 3 ** 2)
-    test("(2^3)^2", (2 ** 3) ** 2)
-    test("2^3+2", 2 ** 3 + 2)
-    test("2^3+5", 2 ** 3 + 5)
-    test("2^9", 2 ** 9)
-    test("sgn(-2)", -1)
-    test("sgn(0)", 0)
-    test("sgn(0.1)", 1)
-    test("foo(0.1)", None)
-    test("round(E, 3)", round(math.e, 3))
-    test("round(PI^2, 3)", round(math.pi ** 2, 3))
-    test("sgn(cos(PI/4))", 1)
-    test("sgn(cos(PI/2))", 0)
-    test("sgn(cos(PI*3/4))", -1)
-    test("+(sgn(cos(PI/4)))", 1)
-    test("-(sgn(cos(PI/4)))", -1)
-    test("hypot(3, 4)", 5)
-    test("multiply(3, 7)", 21)
-    test("all(1,1,1)", True)
-    test("all(1,1,1,1,1,0)", False)
+    test("gD = 9.7543", "gD", TEST.gD)
+    test("A = gD * 9.7543", "A", TEST.A)
+    test("B = 9.7543^2 + gD", "B", TEST.B)
+    test("C = 9.7543 + 2 - 4*A", "C", TEST.C)
+    test("D = 9.7543 + sin(0.3)", "D", TEST.D)
+    test("E = 9.7543 + sin(0.3)", "E", TEST.E)
+    test("alphaD = gD^2 / (4 * PI)", "alphaD", TEST.alphaD)
+    test("sinx = sin(PI/3) - 8.", "sinx", TEST.sinx)
+    test("number = sinx * 5.35e6", "number", TEST.sinx * 5.35e6)
+    test("number2 = gD^2 * 5.35e6", "number", TEST.gD * 5.35e6)
+    test("exp = exp(3*PI)", "exp", math.exp(3 * math.pi))
+    test("exp_0 = exp(3*PI)", "exp_0", math.exp(3 * math.pi))
+    test("24ff = -10+tan(PI/4)^2", "24ff", -10 + math.tan(math.pi / 4) ** 2)
+    test("ff26 = -10+tan(PI/4)^2", "ff24", -10 + math.tan(math.pi / 4) ** 2)
+
+    # print stored variables
+    for k, v in parser.parameters.items():
+        print(k, "=", v)
+
+
+    # test("9", 9)
+    # test("-9", -9)
+    # test("--9", 9)
+    # test("-E", -math.e)
+    # test("9 + 3 + 6", 9 + 3 + 6)
+    # test("9 + 3 / 11", 9 + 3.0 / 11)
+    # test("(9 + 3)", (9 + 3))
+    # test("(9+3) / 11", (9 + 3.0) / 11)
+    # test("9 - 12 - 6", 9 - 12 - 6)
+    # test("9 - (12 - 6)", 9 - (12 - 6))
+    # test("2*3.14159", 2 * 3.14159)
+    # test("3.1415926535*3.1415926535 / 10", 3.1415926535 * 3.1415926535 / 10)
+    # test("PI * PI / 10", math.pi * math.pi / 10)
+    # test("PI*PI/10", math.pi * math.pi / 10)
+    # test("PI^2", math.pi ** 2)
+    # test("round(PI^2)", round(math.pi ** 2))
+    # test("6.02E23 * 8.048", 6.02e23 * 8.048)
+    # test("e / 3", math.e / 3)
+    # test("sin(PI/2)", math.sin(math.pi / 2))
+    # test("10+sin(PI/4)^2", 10 + math.sin(math.pi / 4) ** 2)
+    # test("trunc(E)", int(math.e))
+    # test("trunc(-E)", int(-math.e))
+    # test("round(E)", round(math.e))
+    # test("round(-E)", round(-math.e))
+    # test("E^PI", math.e ** math.pi)
+    # test("exp(0)", 1)
+    # test("exp(1)", math.e)
+    # test("2^3^2", 2 ** 3 ** 2)
+    # test("(2^3)^2", (2 ** 3) ** 2)
+    # test("2^3+2", 2 ** 3 + 2)
+    # test("2^3+5", 2 ** 3 + 5)
+    # test("2^9", 2 ** 9)
+    # test("sgn(-2)", -1)
+    # test("sgn(0)", 0)
+    # test("sgn(0.1)", 1)
+    # test("foo(0.1)", None)
+    # test("round(E, 3)", round(math.e, 3))
+    # test("round(PI^2, 3)", round(math.pi ** 2, 3))
+    # test("sgn(cos(PI/4))", 1)
+    # test("sgn(cos(PI/2))", 0)
+    # test("sgn(cos(PI*3/4))", -1)
+    # test("+(sgn(cos(PI/4)))", 1)
+    # test("-(sgn(cos(PI/4)))", -1)
+    # test("hypot(3, 4)", 5)
+    # test("multiply(3, 7)", 21)
+    # test("all(1,1,1)", True)
+    # test("all(1,1,1,1,1,0)", False)
