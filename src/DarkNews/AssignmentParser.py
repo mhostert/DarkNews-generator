07de13fa9acdd08268a2fc9ebaff13f4ff4744e3
Qubitol <d.massaro.26@gmail.com>
2022-02-17 18:10:12 +0100

    Add support to scanString and multi assignment

--- src/DarkNews/AssignmentParser.py
+++ src/DarkNews/AssignmentParser.py
@@ -116,10 +116,6 @@ class AssignmentParser:
             expr = Forward()
             expr_list = delimitedList(Group(expr))
 
-            # True or False
-            true = CaselessKeyword("True")
-            false = CaselessKeyword("False")
-
             # Quoted strings
             quoted_str_class = QuotedString(quoteChar="\"", escChar="\\", multiline=True, unquoteResults=False) # use the quotes to separate strings from variables
             quoted_str = quoted_str_class.setParseAction(self._push_first)
@@ -148,8 +144,6 @@ class AssignmentParser:
             # _push_first should be given only to the full match otherwise if given when it matches singularly then it would repeat everything every time it backtracks
             assignment_expr <<= (ident + equalop + Or([
                 quoted_str,
-                true.setParseAction(lambda toks: self._current_stack.append(True)),
-                false.setParseAction(lambda toks: self._current_stack.append(False)),
                 list_pattern.setParseAction(self._insert_list_argcount_tuple, self._push_first),
                 expr # this is the most-specific patterns should be placed ahead
             ])).setParseAction(self._push_first)
@@ -196,6 +190,10 @@ class AssignmentParser:
             return math.pi  # 3.1415926535
         elif op == "E":
             return math.e  # 2.718281828
+        elif op.lower() == "true":
+            return True
+        elif op.lower() == "false":
+            return False    
         elif op == "str": # so far str will consume only one argument
             return str(s.pop())
         elif op in self.fn.keys():
@@ -223,16 +221,19 @@ class AssignmentParser:
             stack = self._current_stack
         var_name = stack.pop()
         # consistency check: should not be equal to E, e, PI, pi, Pi, or any other function name
-        if (var_name.lower() in ["e", "pi"]) or (var_name in self.fn.keys()):
+        if (var_name.lower() in ["e", "pi", "true", "false"]) or (var_name in self.fn.keys()):
             raise self.ParsingError(f"Variable name '{var_name}' not allowed.")
         # evaluate the rest of the stack and assign the result to the variable
         self.parameters[var_name] = self._evaluate_expression(stack) # if evaluation contains the new variable which we are trying to assign, everything will fail by construction
         return var_name, self.parameters[var_name]
 
     def parse_string(self, *args, **kwargs):
-        # clean the stack
         self._current_stack[:] = []
         return self._bnf.parseString(*args, **kwargs)
+
+    def scan_string(self, *args, **kwargs):
+        self._current_stack[:] = []
+        return self._bnf.scanString(*args, **kwargs)
 
 
 if __name__ == "__main__":
@@ -289,14 +290,14 @@ if __name__ == "__main__":
     12 * exp( -2 * abs(hbar) )''', "multi_line", TEST.a_variable)
     test("s_1 = \"hello world\"", "s_1", "hello world")
     test("s_2 = \"hello world\" \"people\"", "s_2", None, should_fail=True)
-    test("test_1 = True", "test_1", True)
+    test("test_1 = true", "test_1", True)
     test("test_2 = False", "test_2", False)
     test("test_3 = True False", "test_3", None, should_fail=True)
     test("test_4 = True \"hello\"", "test_4", None, should_fail=True)
     test("test_5 = exp(3*PI)*c True \"hello\"", "test_5", None, should_fail=True)
     test("test_6 = exp(3*PI)*c + True - \"hello\"", "test_6", None, should_fail=True)
-    test("test_7 = exp(3*PI)*c + True", "test_7(*)", None, should_fail=True)
-    test("test_8 = True + exp(3*PI)*c", "test_8", None, should_fail=True)
+    test("test_7 = exp(3*PI)*c + True", "test_7", math.exp(3*math.pi)*TEST.c + True) # it's ok, because float + bool is the bool converted to integer
+    test("test_8 = False + exp(3*PI)*c", "test_8", False + math.exp(3*math.pi)*TEST.c)
     test("list_1 = [-10, 2.3]", "list_1", [-10, 2.3])
     test("list_2 = [-10+tan(PI/4)^2, exp(3*PI)*E]", "list_2", [-10+math.tan(math.pi/4)**2, math.exp(3*math.pi)*math.e])
     test("list_3 = [\"hello\", exp(3*PI)*E]", "list_3", ["hello", math.exp(3*math.pi)*math.e])
@@ -317,4 +318,48 @@ Donec interdum, ex et fermentum aliquam, ante magna convallis magna, et molestie
     # print stored variables
     print("\nStored variables")
     for k, v in parser.parameters.items():
+        print(k, "=", v, type(v))
+
+    # test multi assignments
+    parser_multi = AssignmentParser({})
+    text = '''
+hbar = 6.582119569e-25
+c = 299792458.0
+
+a_variable = -10+tan(PI/4)^2
+a_list = [
+                \"hello\",
+                a_variable,
+                \"world\",
+                \"Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+Nunc ullamcorper blandit nibh, vitae congue lacus convallis at.
+Donec interdum, ex et fermentum aliquam, ante magna convallis magna, et molestie quam quam et dolor.\",
+                c^2 * 3.2e-4 / sin(PI/7) + 
+    12 * exp( -2 * abs(hbar) )
+            ]
+
+
+sinx = sin(PI/3) - 8.
+
+a_multi_line_expression = c^2 * 3.2e-4 / sin(PI/7) + 
+    12 * exp( -2 * abs(hbar) )
+a_bool = True
+
+a_string = \"hello world\"
+    '''
+
+    matches = parser_multi.scan_string(text)
+    for _ in matches:
+        try:
+            print(parser_multi._current_stack)
+            name, val = parser_multi.evaluate_stack(copy=True)
+        except ParseException as pe:
+            print("failed parse:", str(pe))
+        except AssignmentParser.ParsingError as e:
+            print("failed eval:", str(e), parser_multi._current_stack)
+        finally:
+            parser_multi._current_stack[:] = []
+
+    print("\nStored variables")
+    for k, v in parser_multi.parameters.items():
         print(k, "=", v, type(v))
