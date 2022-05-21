f37601327a21883b0abc8fa573b6095bf241e301
mhostert <mhostert@umn.edu>
2022-05-21 17:19:46 -0400

    update tests of output printers

--- tests/test_file_outputs.py
+++ tests/test_file_outputs.py
@@ -8,23 +8,23 @@ import numpy as np
 import DarkNews as dn
 
 
-def test_output_files(light_DP_gen):
+def test_output(light_DP_gen):
     
     df = light_DP_gen
     # check filename exists
-    filename = df.attrs['data_path']
-    assert filename
-    assert os.path.isfile(filename)
-    assert os.access(filename, os.R_OK)
+    gen_path = df.attrs['data_path']
+    
+    assert gen_path
+    assert os.path.isdir(gen_path)
+    file_formats = ['pandas_df.pckl','pandas_df.parquet','ndarray.npy','HEPevt.dat']
+    for format in file_formats:
+        assert os.path.isfile(f'{gen_path}{format}')
+        assert os.access(f'{gen_path}{format}', os.R_OK)
 
     # loading information
-    df_std = pd.read_pickle(f"{filename}pandas_df.pckl")
-    assert df_std
-    df_pq = pd.read_parquet(f"{filename}pandas_df.parquet", engine='pyarrow')
-    assert df_pq
-    nda = np.load(f"{filename}ndarray.npy")
-    assert nda
-
+    df_std = pd.read_pickle(f"{gen_path}pandas_df.pckl")
+    df_pq = pd.read_parquet(f"{gen_path}pandas_df.parquet", engine='pyarrow')
+    nda = np.load(f"{gen_path}ndarray.npy")
 
     # test that numpy array and dataframe formats save the same information
     assert (df_std.to_numpy()[nda!=0]/nda[nda!=0]!=1).sum() == 0 
@@ -38,5 +38,4 @@ def test_MB_rates_of_BPs(light_DP_gen):
     
     df = light_DP_gen
 
-    assert np.abs(np.sum(df.w_event_rate) - 
-
+    assert np.abs(np.sum(df.w_event_rate) - np.sum(df.w_event_rate)) <1e-4
