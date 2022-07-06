bfea84fc757779ff9ef299ecbe968be617985837
mhostert <mhostert@umn.edu>
2022-07-06 11:29:26 -0400

    ubuntu removed from tests -- misbehaving random seed

--- src/DarkNews/fourvec.py
+++ src/DarkNews/fourvec.py
@@ -58,14 +58,14 @@ def df_dot4(dx,dy):
 def df_dot3(dx,dy):
 	return Cfv.dot3(dx.to_numpy(), dy.to_numpy())
 
-def df_cos_opening_angle(dx,dy):
+def df_cos_opening_angle(dx, dy):
 	return Cfv.get_cos_opening_angle(dx.to_numpy(),dy.to_numpy())
 
 def df_cos_azimuthal(dx):
 	return cos_azimuthal(dx.to_numpy())
 
-def df_inv_mass(dx,dy):
-	return inv_mass(dx.to_numpy(),dy.to_numpy())
+def df_inv_mass(dx, dy):
+	return inv_mass(dx.to_numpy(), dy.to_numpy())
 
 
 ##############################
